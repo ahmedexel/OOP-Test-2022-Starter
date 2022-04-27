2a47741337ac4dd0a5bedd5aa35189867c652ac0
ahmedexel <c20439752@mytublin.ie>
2022-04-27 16:35:04 +0100

    generate GetterandSetters

--- src/ie/tudublin/nematode.java
+++ src/ie/tudublin/nematode.java
@@ -8,6 +8,40 @@ public class nematode
     private char gender;
     private boolean eyes;
 
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public float getLength() {
+        return length;
+    }
+    public void setLength(float length) {
+        this.length = length;
+    }
+    public boolean isLimbs() {
+        return limbs;
+    }
+    public void setLimbs(boolean limbs) {
+        this.limbs = limbs;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public boolean isEyes() {
+        return eyes;
+    }
+    public void setEyes(boolean eyes) {
+        this.eyes = eyes;
+    }
+
+    
+
 
 }
 
