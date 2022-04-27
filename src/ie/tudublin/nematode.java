8c5717bcafd413327af6861361dcefe35199b778
ahmedexel <c20439752@mytublin.ie>
2022-04-27 16:33:18 +0100

    onto GettersandSetters

---
+++ src/ie/tudublin/nematode.java
@@ -0,0 +1,14 @@
+package ie.tudublin;
+
+public class nematode
+{
+    private String name;
+    private float length;
+    private boolean limbs;
+    private char gender;
+    private boolean eyes;
+
+
+}
+
+
