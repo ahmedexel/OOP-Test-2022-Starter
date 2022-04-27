acb2aa1211274f6e850d9d5f57f8b98fb4f2c323
ahmedexel <c20439752@mytublin.ie>
2022-04-27 17:17:23 +0100

    fixed file misplacement

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -1,30 +1,35 @@
 package ie.tudublin;
 
-public class nematode
+import processing.data.TableRow;
+
+public class Nematode
 {
     private String name;
     private float length;
     private boolean limbs;
-    private char gender;
+    private String gender;
     private boolean eyes;
 
 
+    @Override
+    public String toString() {
+        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs + ", name="
+                + name + "]";
+    }
 
-
-    public nematode(TableRow tr)
+    public Nematode(TableRow tr)
     {
         this(
             tr.getString("name"), 
             tr.getFloat("length"),
             tr.getInt("limbs") == 1, 
-            tr.getchar("gender"),
+            tr.getString("gender"),
             tr.getInt("eyes") == 1
         );
     }
 
 
-
-    public nematode(String name, float length, boolean limbs, char gender, boolean eyes) {
+    public Nematode(String name, float length, boolean limbs, String gender, boolean eyes) {
         this.name = name;
         this.length = length;
         this.limbs = limbs;
@@ -32,41 +37,92 @@ public class nematode
         this.eyes = eyes;
     }
 
-    
+
+
     public String getName() {
         return name;
     }
+
+
+
+
+
+
     public void setName(String name) {
         this.name = name;
     }
+
+
+
+
+
+
     public float getLength() {
         return length;
     }
+
+
+
+
+
+
     public void setLength(float length) {
         this.length = length;
     }
+
+
+
+
+
+
     public boolean isLimbs() {
         return limbs;
     }
+
+
+
+
+
+
     public void setLimbs(boolean limbs) {
         this.limbs = limbs;
     }
-    public char getGender() {
+
+
+
+
+
+
+    public String getGender() {
         return gender;
     }
-    public void setGender(char gender) {
+
+
+
+
+
+
+    public void setGender(String gender) {
         this.gender = gender;
     }
+
+
+
+
+
+
     public boolean isEyes() {
         return eyes;
     }
+
+
+
+
+
+
     public void setEyes(boolean eyes) {
         this.eyes = eyes;
     }
-
-    
-
-
 }
 
 
